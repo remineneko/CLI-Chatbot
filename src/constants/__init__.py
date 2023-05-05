35545dabbe7ec993e22846d3f110a32ad0c9e44f
HoangTranUnion <henry11102000@gmail.com>
2023-05-05 13:02:04 +0700

    added connstants

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,4 +1,8 @@
 from .bot_config import ChatbotConfig
-import os
 
-CHATBOT_API_TOKEN = ChatbotConfig().api_token
+config = ChatbotConfig()
+
+CHATBOT_API_TOKEN = config.api_token
+DATABASE_HOST = config.db_host
+DATABASE_USERNAME = config.db_username
+DATABASE_PASSWORD = config.db_password
