f71a8b398e27460caa6882ceb19f547364315719
HoangTranUnion <henry11102000@gmail.com>
2023-05-05 13:00:50 +0700

    revamped how the program handles the configs and added a simple database

--- src/constants/bot_config.py
+++ src/constants/bot_config.py
@@ -6,57 +6,43 @@ from constants.exceptions import *
 import os
 import logging
 
-ENVIRONMENT_NAME = 'OPENAI_API_KEY'
-
+OPENAI_KEY = 'OPENAI_API_KEY'
+DB_NAME = "DB_NAME"
+DB_USERNAME = "DB_USERNAME"
+DB_PASSWORD = "DB_PASSWORD"
 
 class ChatbotConfig:
     """
     Sets up basic configurations for the bot.
     """
-    def __init__(self, config_file: Union[str, Path] = "bot_core_config.ini"):
+    def __init__(self):
         self._config_logger = logging.getLogger(__name__)
         self._config_logger.setLevel(logging.INFO)
         self._config_logger.addHandler(ChatbotHandler())
         
         self._config_logger.info("Setting up the bot... Please wait a moment.")
-        self._config_file = config_file
-        if not os.path.isfile(config_file):
-            self._config_logger.info("Config file not found, creating a new one.")
-            self._create_new_config(self.__first_time_run())
-            self._config_logger.info("Config file created. Continuing setting up the bot...")
 
-        self._data = self._read_config()
-        
-    def __first_time_run(self):
-        if ENVIRONMENT_NAME in os.environ:
-            return os.environ[ENVIRONMENT_NAME]
+    def __check_existence(self, key):
+        if key in os.environ:
+            return os.environ[key]
         else:
-            raise EnvironmentVariableNotFoundException("The environment variable must be manually set before using this program.")
-
-    def _create_new_config(self, api_key: str):
-        config = configparser.ConfigParser()
-        config['DEFAULT'] = {
-            'API_KEY': api_key
-        }
-        
-        with open(self._config_file, 'w') as cf:
-            config.write(cf)
-
-    def _read_config(self):
-        config = configparser.ConfigParser()
-        config.read(self._config_file)
-        return self.__verify(config['DEFAULT']['API_KEY'])
-
-    def __verify(self, api_key:str):
-        if ENVIRONMENT_NAME in os.environ:
-            return api_key if api_key == os.environ[ENVIRONMENT_NAME] else None
-        else:
-            raise EnvironmentVariableNotFoundException("The environment variable must be manually set before using this script.")
+            raise EnvironmentVariableNotFoundException("The environment variables must be manually set before using this program.")
 
     @property
     def api_token(self):
-        if not self._data:
-            raise ModificationFoundException("The config file has been manually edited - please check the key in the config file.")
-        else:
-            return self._data
+        return self.__check_existence(OPENAI_KEY)
+    
+    @property
+    def db_host(self):
+        return self.__check_existence(DB_NAME)
+    
+    @property
+    def db_username(self):
+        return self.__check_existence(DB_USERNAME)
+    
+    @property
+    def db_password(self):
+        return self.__check_existence(DB_PASSWORD)
+        
+
 
---
+++ src/database/database.py
@@ -0,0 +1,58 @@
+import mysql.connector
+
+class DBSession:
+    def __init__(self, host, user_name, password):
+        self.access = mysql.connector.connect(
+            host = host,
+            user = user_name,
+            password = password,
+            use_unicode = True,
+            charset = 'utf8'
+        )
+
+        self.dbcursor = self.access.cursor(buffered = True)
+        self._db_name = "Chatbot"
+        self._create_database()
+        self._create_table()
+
+    def _create_database(self):
+        self.dbcursor.execute(f"CREATE DATABASE IF NOT EXISTS {self._db_name};")
+        self.access.commit()
+
+    def _use_db(self, cmd, args = None):
+        self.dbcursor.execute(f"USE {self._db_name};")
+        self.access.commit()
+
+        if args:
+            self.dbcursor.execute(cmd, args)
+        else:
+            self.dbcursor.execute(cmd)
+        self.access.commit()
+
+    def _create_table(self):
+        cmd = "CREATE TABLE IF NOT EXISTS ChatSession (chat_id INT NOT NULL AUTO_INCREMENT, created_date DATETIME, updated_date DATETIME, history_file_name VARCHAR(255), PRIMARY KEY (chat_id));"
+        self._use_db(cmd)
+
+    def save_session(self, file_name):
+        cmd = f"INSERT IGNORE INTO ChatSession (created_date, updated_date, history_file_name) VALUES (NOW(), NOW(), %(history_file_name)s);"
+        args = {
+            "history_file_name": file_name
+        }
+        self._use_db(cmd, args)
+
+    def update_history(self, file_name):
+        cmd = "UPDATE ChatSession SET updated_date = NOW() WHERE history_file_name=%(history_file_name)s;"
+        args = {
+            "history_file_name": file_name
+        }
+        self._use_db(cmd, args)
+
+    def get_history(self):
+        cmd = "SELECT * FROM ChatSession;"
+        self._use_db(cmd)
+        entries = self.dbcursor.fetchall()
+        return entries
+    
+    def clear_table(self):
+        cmd = "TRUNCATE ChatSession;"
+        self._use_db(cmd)
