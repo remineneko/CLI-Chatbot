16ab9de8231dc31fdc6b69eadcb241fca61b578a
HoangTranUnion <henry11102000@gmail.com>
2023-05-10 16:49:46 +0700

    added a naive llm endpoint

---
+++ src/endpoints/llm_models.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from .source import Source
+from .output import ChatOutput
+
+
+class LLMModel(ABC):
+    def __init__(self, model, **kwargs):
+        """
+        Initialize the LLMModel class.
+
+        Args:
+            model_name (_type_): _description_
+            required_imports (tuple, optional): _description_. Defaults to (None, None).
+        """
+        self.model_name = model
+        self.__dict__.update(kwargs)
+
+    @abstractmethod
+    def generate(self, prompt: Source) -> ChatOutput:
+        pass
