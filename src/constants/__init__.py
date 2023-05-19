6f0e26584f28239d8c412be2df2b41838ac70d53
HoangTranUnion <henry11102000@gmail.com>
2023-05-19 21:38:35 +0700

    removed some unused constants

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -5,10 +5,7 @@ from dir_constants import ROOT_FOLDER
 
 config = ChatbotConfig()
 
-CHATBOT_API_TOKEN = config.api_token
-DATABASE_HOST = config.db_host
-DATABASE_USERNAME = config.db_username
-DATABASE_PASSWORD = config.db_password
+#CHATBOT_API_TOKEN = config.api_token
 
 PERSIST_DB_FOLDER = os.path.join(ROOT_FOLDER, 'chromadb')
 if not os.path.isdir(PERSIST_DB_FOLDER):
--- src/constants/bot_config.py
+++ src/constants/bot_config.py
@@ -1,12 +1,8 @@
-from constants.exceptions import *
+from .exceptions import *
 import os
-import logging
 from src.logger import ChatbotLogger
 
 OPENAI_KEY = 'OPENAI_API_KEY'
-DB_NAME = "DB_NAME"
-DB_USERNAME = "DB_USERNAME"
-DB_PASSWORD = "DB_PASSWORD"
 
 class ChatbotConfig:
     """
@@ -21,24 +17,12 @@ class ChatbotConfig:
             self._config_logger.info(f"Environment variable {key} has been found.")
             return os.environ[key]
         else:
-            self._config_logger.critical(f"Environment variable {key} does not exist.")
+            self._config_logger.error(f"Environment variable {key} does not exist.")
             raise EnvironmentVariableNotFoundException("The environment variables must be manually set before using this program.")
 
     @property
     def api_token(self):
         return self.__check_existence(OPENAI_KEY)
-    
-    @property
-    def db_host(self):
-        return self.__check_existence(DB_NAME)
-    
-    @property
-    def db_username(self):
-        return self.__check_existence(DB_USERNAME)
-    
-    @property
-    def db_password(self):
-        return self.__check_existence(DB_PASSWORD)
         
 
 
